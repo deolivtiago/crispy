da89278bd1bdffe80368ad22806e393cea0bd918
Tiago Oliveira <olvrti@gmail.com>
2021-11-16 19:22:13 -0300

    chore: add spring web 2.5

---
+++ src/main/java/com/github/olvrti/crispy/CrispyApplication.java
@@ -0,0 +1,13 @@
+package com.github.olvrti.crispy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrispyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrispyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/olvrti/crispy/CrispyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.olvrti.crispy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrispyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
