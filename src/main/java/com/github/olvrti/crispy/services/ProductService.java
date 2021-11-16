02c848a6394211733006a369e81b4593ca3e11b1
Tiago Oliveira <olvrti@gmail.com>
2021-11-16 20:42:54 -0300

    feat: add Product service

---
+++ src/main/java/com/github/olvrti/crispy/services/ProductService.java
@@ -0,0 +1,41 @@
+package com.github.olvrti.crispy.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.github.olvrti.crispy.domain.Product;
+import com.github.olvrti.crispy.repositories.ProductRepository;
+import com.github.olvrti.crispy.services.exceptions.ObjectNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+  @Autowired
+  private ProductRepository repository;
+
+  public List<Product> findAll() {
+    return repository.findAll();
+  }
+
+  public Product find(final Integer id) {
+    final Optional<Product> product = repository.findById(id);
+    return product.orElseThrow(() -> new ObjectNotFoundException(String.format("Object not found: %s", id)));
+  }
+
+  public Product create(final Product product) {
+    return repository.save(product);
+  }
+
+  public Product update(final Product product) {
+    find(product.getId());
+    return repository.save(product);
+  }
+
+  public void delete(final Integer id) {
+    find(id);
+    repository.deleteById(id);
+  }
+
+}
---
+++ src/main/java/com/github/olvrti/crispy/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,13 @@
+package com.github.olvrti.crispy.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException {
+  private static final long serialVersionUID = 1L;
+
+  public ObjectNotFoundException(final String message) {
+    super(message);
+  }
+
+  public ObjectNotFoundException(final String message, final Throwable cause) {
+    super(message, cause);
+  }
+}
